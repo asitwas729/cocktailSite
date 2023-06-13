67f5f564b40b35935b8330239bd96841b00de79a
Yang Hyemin <asitwas729@gmail.com>
2023-06-13 20:41:36 +0900

    update

---
+++ build/generated/querydsl/com/example/yanghyemin/entity/QBoard.java
@@ -0,0 +1,49 @@
+package com.example.yanghyemin.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QBoard is a Querydsl query type for Board
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBoard extends EntityPathBase<Board> {
+
+    private static final long serialVersionUID = 128015161L;
+
+    public static final QBoard board = new QBoard("board");
+
+    public final StringPath contents = createString("contents");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath title = createString("title");
+
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = createDateTime("updatedAt", java.time.LocalDateTime.class);
+
+    public final StringPath userId = createString("userId");
+
+    public final StringPath userName = createString("userName");
+
+    public QBoard(String variable) {
+        super(Board.class, forVariable(variable));
+    }
+
+    public QBoard(Path<? extends Board> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBoard(PathMetadata metadata) {
+        super(Board.class, metadata);
+    }
+
+}
+
---
+++ build/generated/querydsl/com/example/yanghyemin/entity/QOrder.java
@@ -0,0 +1,51 @@
+package com.example.yanghyemin.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QOrder is a Querydsl query type for Order
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QOrder extends EntityPathBase<Order> {
+
+    private static final long serialVersionUID = 140112801L;
+
+    public static final QOrder order = new QOrder("order1");
+
+    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final NumberPath<Integer> price = createNumber("price", Integer.class);
+
+    public final NumberPath<Long> productId = createNumber("productId", Long.class);
+
+    public final StringPath productName = createString("productName");
+
+    public final DateTimePath<java.time.LocalDateTime> updatedAt = createDateTime("updatedAt", java.time.LocalDateTime.class);
+
+    public final StringPath userId = createString("userId");
+
+    public final StringPath userName = createString("userName");
+
+    public QOrder(String variable) {
+        super(Order.class, forVariable(variable));
+    }
+
+    public QOrder(Path<? extends Order> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QOrder(PathMetadata metadata) {
+        super(Order.class, metadata);
+    }
+
+}
+
