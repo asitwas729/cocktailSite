1165839124ff66e10b7c607326c0ba85eaec3161
Yang Hyemin <asitwas729@gmail.com>
2023-06-18 13:45:20 +0900

    update

--- src/main/java/com/example/yanghyemin/config/SecurityConfig.java
+++ src/main/java/com/example/yanghyemin/config/SecurityConfig.java
@@ -15,7 +15,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 
 @Configuration
 @EnableGlobalMethodSecurity(
-        prePostEnabled = true
+    prePostEnabled = true
 )
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
@@ -29,20 +29,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity httpSecurity) throws Exception {
 
         httpSecurity.httpBasic().disable().csrf().disable().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/sign-api/sing-in", "/sign-api/sing-up", "/sign-api/exception").permitAll()
-                .antMatchers(HttpMethod.GET, "/user/**").permitAll() //
-                .antMatchers("**exception**").permitAll()
-                .antMatchers("/product/**").permitAll()
-                .antMatchers("/board/**").permitAll()
-                .antMatchers("/order/**").permitAll()
-                .anyRequest().hasAnyRole("USER","ADMIN")
-                .and()
-                .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
-                .and()
-                .exceptionHandling().authenticationEntryPoint(new CustomAuthenicationEntryPoint())
-                .and()
-                .addFilterBefore(new JwtAuthenicationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
+            .authorizeRequests().antMatchers("/sign-api/sing-in", "/sign-api/sing-up", "/sign-api/exception", "/cocktail/list", "/cocktail/createdAt", "/cocktail/byName", "/cocktail/ingredients", "/cocktail/search", "/cocktail/alcohol","/cocktail/id").permitAll()
+            .antMatchers(HttpMethod.GET, "/user/**").permitAll() //
+            .antMatchers(HttpMethod.GET, "/cocktail/**").permitAll()
+            .antMatchers("**exception**").permitAll()
+            .antMatchers("/product/**").permitAll()
+            .antMatchers("/board/**").permitAll()
+            .antMatchers("/order/**").permitAll()
+            .anyRequest().hasAnyRole("USER","ADMIN")
+            .and()
+            .exceptionHandling().accessDeniedHandler(new CustomAccessDeniedHandler())
+            .and()
+            .exceptionHandling().authenticationEntryPoint(new CustomAuthenicationEntryPoint())
+            .and()
+            .addFilterBefore(new JwtAuthenicationFilter(jwtTokenProvider), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -50,3 +51,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         webSecurity.ignoring().antMatchers("/v2/api-docs", "/v3/api-docs/**", "/swagger-resources/**", "/swagger-ui/**","/swagger-ui/index.html", "/webjars/**", "/swagger/**", "/sign-api/exception");
     }
 }
+
+
+
+
+
+
--- src/main/java/com/example/yanghyemin/controller/SignController.java
+++ src/main/java/com/example/yanghyemin/controller/SignController.java
@@ -4,6 +4,7 @@ import com.example.yanghyemin.dto.SignInResultDto;
 import com.example.yanghyemin.dto.SignUpResultDto;
 import com.example.yanghyemin.service.SignService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
